d7a55d314f28aaf46b8e614144e4a749b8d8dd41
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-08-14 22:21:24 +0200

    Transform data

--- src/services/restaurant/RestaurantService.tsx
+++ src/services/restaurant/RestaurantService.tsx
@@ -1,11 +1,27 @@
+import { Restaurant, RestaurantResponse } from '@/src/utils/models';
+import camelize from 'camelize';
 import { mocks } from './mock';
 
 export const restaurantRequest = (location = '37.7749295,-122.4194155') => {
-  return new Promise((resolve, reject) => {
+  return new Promise<RestaurantResponse>((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
       reject('not found');
     }
-    resolve(mock);
+    resolve(mock as RestaurantResponse);
   });
+};
+
+export const restaurantTransform = ({
+  results = [],
+}: RestaurantResponse): Restaurant => {
+  const mapResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY',
+    };
+  });
+
+  return camelize(mapResults);
 };
--- src/utils/models.ts
+++ src/utils/models.ts
@@ -21,3 +21,113 @@ export const TAB_ICON = {
 };
 
 export type TabValue = 'restaurant' | 'settings' | 'map';
+
+// ---------------- Restaurant ---------------- //
+
+export interface RestaurantResponse {
+  html_attributions: string[];
+  next_page_token: string;
+  results: Results[];
+  status: string;
+}
+
+export interface Results {
+  business_status: string;
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+    viewport: {
+      northeast: {
+        lat: number;
+        lng: number;
+      };
+      southwest: {
+        lat: number;
+        lng: number;
+      };
+    };
+  };
+  icon: string;
+  name: string;
+  opening_hours?: {
+    open_now: boolean;
+  };
+  photos: {
+    height: number;
+    html_attributions: string[];
+    photo_reference: string;
+    width: number;
+  }[];
+  place_id: string;
+  plus_code: {
+    compound_code: string;
+    global_code: string;
+  };
+  price_level?: number;
+  rating: number;
+  reference: string;
+  scope: string;
+  types: string[];
+  user_ratings_total: number;
+  vicinity: string;
+  permanently_closed?: boolean;
+}
+
+export interface TRestaurantResponse {
+  htmlAttributions: string[];
+  nextPageToken: string;
+  results: Restaurant[];
+  status: string;
+}
+
+export interface Restaurant {
+  businessStatus: string;
+  geometry: Geometry;
+  icon: string;
+  name: string;
+  openingHours?: OpeningHours;
+  photos: Photo[];
+  placeId: string;
+  plusCode: PlusCode;
+  priceLevel?: number;
+  rating: number;
+  reference: string;
+  scope: string;
+  types: string[];
+  userRatingsTotal: number;
+  vicinity: string;
+  permanentlyClosed?: boolean;
+}
+
+interface Geometry {
+  location: Location;
+  viewport: Viewport;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+}
+
+interface Viewport {
+  northeast: Location;
+  southwest: Location;
+}
+
+interface OpeningHours {
+  openNow: boolean;
+}
+
+interface Photo {
+  height: number;
+  htmlAttributions: string[];
+  photoReference: string;
+  width: number;
+}
+
+interface PlusCode {
+  compoundCode: string;
+  globalCode: string;
+}

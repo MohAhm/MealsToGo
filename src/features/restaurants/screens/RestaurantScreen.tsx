02cc67d835999fa5ccd194637aa73eef3cd1f58f
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-08-18 21:31:53 +0200

    Add Activity Indicator

--- src/features/restaurants/screens/RestaurantScreen.tsx
+++ src/features/restaurants/screens/RestaurantScreen.tsx
@@ -3,7 +3,7 @@ import { RestaurantContext } from '@/src/services/restaurant/RestaurantContext';
 import { Restaurant } from '@/src/utils/models';
 import React, { useContext, useState } from 'react';
 import { FlatList, ListRenderItemInfo } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { ActivityIndicator, MD2Colors, Searchbar } from 'react-native-paper';
 import styled from 'styled-components/native';
 import { RestaurantInfoCard } from '../components/RestaurantInfoCard/RestaurantInfoCard';
 
@@ -15,12 +15,27 @@ const RestaurantList = styled(FlatList<Restaurant>).attrs({
   contentContainerStyle: { padding: 16 },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantScreen = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const { restaurants } = useContext(RestaurantContext);
+  const { isLoading, restaurants } = useContext(RestaurantContext);
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading animating={true} color={MD2Colors.blue300} size={50} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar
           placeholder="Search"

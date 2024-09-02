06fe8a58201efebfffe568f8781fee5281518e55
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-09-02 22:24:01 +0200

    Restructuring Navigation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,48 +3,15 @@ import {
   Oswald_400Regular,
   useFonts as useOswald,
 } from '@expo-google-fonts/oswald';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { RouteProp } from '@react-navigation/native';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
-import { SafeArea } from '../components/utility/SafeArea';
-import { RestaurantScreen } from '../features/restaurants/screens/RestaurantScreen';
 import { theme } from '../infrastructure/theme';
+import { Navigation } from '../infrastructure/theme/navigation';
 import { LocationProvider } from '../services/location/LocationContext';
 import { RestaurantProvider } from '../services/restaurant/RestaurantContext';
-import { BottomTabParamList, TAB_ICON, TabValue } from '../utils/models';
-
-type BottomTabRouteProp = RouteProp<BottomTabParamList>;
-
-const Tab = createBottomTabNavigator<BottomTabParamList>();
-
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
-  </SafeArea>
-);
-
-const createScreenOptions = ({ route }: { route: BottomTabRouteProp }) => {
-  const iconName = TAB_ICON[route.name];
-  return {
-    headerShown: false,
-    tabBarIcon: ({ size, color }: { size: number; color: string }) => (
-      <Ionicons name={iconName as TabValue} size={size} color={color} />
-    ),
-    tabBarActiveTintColor: 'tomato',
-    tabBarInactiveTintColor: 'gray',
-  };
-};
 
 export default function Index() {
   const [oswaldLoaded, oswaldError] = useOswald({
@@ -70,11 +37,7 @@ export default function Index() {
       <SafeAreaProvider>
         <LocationProvider>
           <RestaurantProvider>
-            <Tab.Navigator screenOptions={createScreenOptions}>
-              <Tab.Screen name="Restaurant" component={RestaurantScreen} />
-              <Tab.Screen name="Map" component={Map} />
-              <Tab.Screen name="Settings" component={Settings} />
-            </Tab.Navigator>
+            <Navigation />
           </RestaurantProvider>
         </LocationProvider>
       </SafeAreaProvider>
---
+++ src/infrastructure/theme/navigation/AppNavigator.tsx
@@ -0,0 +1,48 @@
+import { SafeArea } from '@/src/components/utility/SafeArea';
+import { RestaurantScreen } from '@/src/features/restaurants/screens/RestaurantScreen';
+import { BottomTabParamList, TAB_ICON, TabValue } from '@/src/utils/models';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RouteProp } from '@react-navigation/native';
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+type BottomTabRouteProp = RouteProp<BottomTabParamList>;
+
+const Tab = createBottomTabNavigator<BottomTabParamList>();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
+
+const createScreenOptions = ({ route }: { route: BottomTabRouteProp }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    headerShown: false,
+    tabBarIcon: ({ size, color }: { size: number; color: string }) => (
+      <Ionicons name={iconName as TabValue} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  };
+};
+
+export const AppNavigator = () => {
+  return (
+    <SafeAreaProvider>
+      <Tab.Navigator screenOptions={createScreenOptions}>
+        <Tab.Screen name="Restaurant" component={RestaurantScreen} />
+        <Tab.Screen name="Map" component={Map} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </SafeAreaProvider>
+  );
+};
